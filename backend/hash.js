f526b7ce0e8d6580a968f8fb7fd002efa56a5640
Ruben Hernandez <rubenhectorh7@gmail.com>
2026-03-13 23:18:39 -0300

    Update hash.js

--- backend/hash.js
+++ backend/hash.js
@@ -1,9 +1,7 @@
 import bcrypt from "bcrypt"
 
-const password = "admin123"
+const password = "ppappa"
 
 const hash = await bcrypt.hash(password, 10)
 
 console.log(hash)
-
-$2b$10$QA1.EY6FRTxwGEHyjE98aeismvAl8e1wNSUd0BGuom78WBwKgVNQW
